2e7dda91fa88306e8b448ad561168ea6a5c312a1
Vladimir Zotov <azuresplash@gmail.com>
2018-09-04 11:44:37 +1000

    Updated build root url to work with github instead

--- config/paths.js
+++ config/paths.js
@@ -54,5 +54,5 @@ module.exports = {
   appTsProdConfig: resolveApp('tsconfig.prod.json'),
   appTsLint: resolveApp('tslint.json'),
   publicUrl: getPublicUrl(resolveApp('package.json')),
-  servedPath: getServedPath(resolveApp('package.json')),
+  servedPath: '/task-my-shows/',//getServedPath(resolveApp('package.json')),
 };
